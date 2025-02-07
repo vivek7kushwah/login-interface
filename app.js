2941f8763e4363a8bf8f70db5f63cf25f41f7c4e
vivek7kushwah <vivekkushwah1412@gmail.com>
2025-02-08 02:39:27 +0530

    some changes

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const cookieParser =require('cookie-parser');
+app.use(cookieParser());
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const fs= require('fs');
+const userModel = require('./usermodules');
+app.use(express.json());
+app.use(express.urlencoded({ extended : true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view.engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render("index.ejs");
+})
+app.post("/login",async(req,res)=>{
+    let user = await userModel.findOne({userid : req.body.id});
+    if (!user) return res.send("something wents wrong...");
+    bcrypt.compare(req.body.password,user.password,function(err,result){
+        if (err) return res.send("internal server error...");
+        if (result){
+            let id = req.body.id;
+            let token = jwt.sign({id},"currentlylogedin");
+            res,cookie("token",token);
+            res.send("you login succesfully...");
+        }
+    });
+    
+})
+app.listen(3000,(err)=>{
+    console.log("server startat");
+})
+
+
 
--- usermodules.js
+++ usermodules.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://127.0.0.1:27017/projectdata`);
 
+const userSchema = mongoose.Schema({
+    userid : String ,
+    password_hash : String ,
+    role : String 
+})
+
+module.exports = mongoose.model("user", userSchema);
