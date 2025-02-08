6e2bdb5c1e55d5d8b2070e5fd26723cc27393a82
vivek kushwah <vivekkushwah1412@gmail.com>
2025-02-09 01:35:19 +0530

    Update usermodules.js
    sapabase

--- usermodules.js
+++ usermodules.js
@@ -1,11 +1,17 @@
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
-mongoose.connect(`mongodb://127.0.0.1:27017/projectdata`,{ useNewUrlParser: true, useUnifiedTopology: true });
+// const mongoose = require('mongoose');
+// mongoose.set('strictQuery', true);
+// mongoose.connect(`mongodb://127.0.0.1:27017/projectdata`,{ useNewUrlParser: true, useUnifiedTopology: true });
 
-const userSchema = mongoose.Schema({
-    userid : String ,
-    password_hash : String ,
-    role : String 
-})
+// const userSchema = mongoose.Schema({
+//     userid : String ,
+//     password_hash : String ,
+//     role : String 
+// })
 
-module.exports = mongoose.model("user", userSchema);
+// module.exports = mongoose.model("user", userSchema);
+import postgres from 'postgres'
+
+const connectionString = process.env.DATABASE_URL
+const sql = postgres(connectionString)
+
+export default sql
