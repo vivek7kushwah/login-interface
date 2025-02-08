dc23135e904a434447b266d09c013ba8f9e4e1cd
vivek7kushwah <vivekkushwah1412@gmail.com>
2025-02-08 15:34:12 +0530

    some changes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const fs= require('fs');
 const userModel = require('./usermodules');
+const { console } = require('inspector');
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
 app.use(express.static(path.join(__dirname,'public')));
@@ -15,11 +16,29 @@ app.set('view.engine','ejs');
 app.get('/',(req,res)=>{
     res.render("index.ejs");
 })
+// let password = "passwordabc";
+// bcrypt.genSalt(10, (err,salt)=>{
+//     bcrypt.hash(password,salt, async(err,hash)=>{
+//         let user = await userModel.create({
+//             userid : "user1",
+//             password_hash : hash,
+//             role : "basic"
+//         })
+//     })
+// })
+
 app.post("/login",async(req,res)=>{
     let user = await userModel.findOne({userid : req.body.id});
     if (!user) return res.send("something wents wrong...");
+    
     bcrypt.compare(req.body.password,user.password,function(err,result){
-        if (err) return res.send("internal server error...");
+        
+        if (err) return res.send({
+            a : req.body.password,
+            b : user.password_hash,
+            c : err
+        });
+        // if (err) return res.send("internal server error");
         if (result){
             let id = req.body.id;
             let token = jwt.sign({id},"currentlylogedin");
--- usermodules.js
+++ usermodules.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb://127.0.0.1:27017/projectdata`);
+mongoose.set('strictQuery', true);
+mongoose.connect(`mongodb://127.0.0.1:27017/projectdata`,{ useNewUrlParser: true, useUnifiedTopology: true });
 
 const userSchema = mongoose.Schema({
     userid : String ,
