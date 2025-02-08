3d884cd33497ff1e9e1befcd040d7c3ee408548a
vivek kushwah <vivekkushwah1412@gmail.com>
2025-02-09 02:08:26 +0530

    Update usermodules.js

--- usermodules.js
+++ usermodules.js
@@ -1,3 +1,24 @@
+const { Client } = require('pg');
+
+// Create a client for the SAPA database
+const client = new Client({
+    host: "db.ccjdcvsbdpechjaufhti.supabase.co",       // e.g., "sapa.example.com"
+    user: "vivek7kushwah",        // Your SAPA database username
+    password: "6116a939",    // Your SAPA database password
+    database: "users",    // The name of your SAPA database
+    port: 5432                    // Optional: Default is 5432 for PostgreSQL
+});
+
+// Connect to the database
+client.connect((err) => {
+    if (err) {
+        console.error("Error connecting to SAPA database:", err);
+        return;
+    }
+    console.log("Connected to SAPA database!");
+});
+
+module.exports = client;
 // const mongoose = require('mongoose');
 // mongoose.set('strictQuery', true);
 // mongoose.connect(`mongodb://127.0.0.1:27017/projectdata`,{ useNewUrlParser: true, useUnifiedTopology: true });
@@ -9,9 +30,9 @@
 // })
 
 // module.exports = mongoose.model("user", userSchema);
-import postgres from 'postgres'
-const DATABASE_URL=postgresql://postgres:[YOUR-PASSWORD]@db.ccjdcvsbdpechjaufhti.supabase.co:5432/postgres
-const connectionString = process.env.DATABASE_URL
-const sql = postgres(connectionString)
+// import postgres from 'postgres'
+// const DATABASE_URL=postgresql://postgres:[YOUR-PASSWORD]@db.ccjdcvsbdpechjaufhti.supabase.co:5432/postgres
+// const connectionString = process.env.DATABASE_URL
+// const sql = postgres(connectionString)
 
-export default sql
+// export default sql
