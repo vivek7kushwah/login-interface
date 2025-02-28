fd595b51a78cda54e992a3afc76bc99113754dd3
vivek7kushwah <vivekkushwah1412@gmail.com>
2025-02-28 18:35:08 +0530

    styling of login page

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const userModel = require('./usermodules');
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static('public')); 
 app.set('view.engine','ejs');
-app.use(express.static(path.join(__dirname,'public')));
+
 
 app.get('/',(req,res)=>{
     res.render("index.ejs");
